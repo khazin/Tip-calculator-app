8483d7d697aee79ee34db7cd087929fd1d615bfc
khazin <khazin316@gmail.com>
2021-07-30 16:56:03 +0800

    done project

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 class TipCalculator {
-  /*input value*/
+  /**
+   * input value
+   */
   _value = "";
   _billValue = "";
   _peopleValue = "";
@@ -7,7 +9,6 @@ class TipCalculator {
   _tipPercValue = "";
 
   constructor() {
-    /*input and output element*/
     this.billInput = document.querySelector("#billInput");
     this.peopleInput = document.querySelector("#peopleInput");
     this.customTipInput = document.querySelector("#customTipInput");
@@ -15,8 +16,12 @@ class TipCalculator {
     this.totalTipOutput = document.querySelector("#totalTipOutput");
     this.peopleInputLabel = document.querySelector(".people--label");
     this.billInputLabel = document.querySelector(".bill--label");
+    this.resetButton = document.querySelector("#resetButton");
   }
-  /*setter and getter methods*/
+
+  /**
+   * setter and getter methods
+   */
   get value() {
     return this._value;
   }
@@ -48,20 +53,25 @@ class TipCalculator {
     this._tipPercValue = value;
   }
 
-  /*store any input values*/
+  /**
+   * store any input values
+   */
   storeValue() {
     document.addEventListener("keyup", (e) => {
       let element = e.target;
       let regex = /^[0-9\.]+$/;
       if (element.tagName == "INPUT") {
-          if (!element.value.match(regex)) {
-              element.value='';
-          }
+        if (!element.value.match(regex)) {
+          element.value = "";
+        }
         this.value = element.value;
       }
     });
   }
-  /*store  bill values*/
+
+  /**
+   * store  bill values
+   */
   setBill() {
     document.addEventListener("keyup", (e) => {
       let element = e.target;
@@ -71,18 +81,9 @@ class TipCalculator {
     });
   }
 
-  /*store custom % tip values*/
-  setCustomPercTip() {
-    document.addEventListener("keyup", (e) => {
-      let element = e.target;
-      if (element.id == "customTipInput") {
-        this.customTipPercValue = this.value;
-        this.tipPercValue = this.customTipPercValue / 100;
-      }
-    });
-  }
-
-  /*store amt of people values*/
+  /**
+   * store amt of people values
+   */
   setPeople() {
     document.addEventListener("keyup", (e) => {
       let element = e.target;
@@ -92,20 +93,37 @@ class TipCalculator {
     });
   }
 
-  /*store % tip value*/
+  /**
+   * store custom % tip values
+   */
+  setCustomPercTip() {
+    document.addEventListener("keyup", (e) => {
+      let element = e.target;
+      if (element.id == "customTipInput") {
+        if (element.value > 100) {
+          element.value = 100;
+        }
+        this.customTipPercValue = this.value;
+        this.tipPercValue = this.customTipPercValue / 100;
+      }
+    });
+  }
+
+  /**
+   * store % tip value
+   */
   setTipPerc() {
     document.addEventListener("click", (e) => {
       let element = e.target;
-      if (
-        element.classList.contains("tip--input") &&
-        element.classList.contains("-button")
-      ) {
+      if (element.classList.contains("tip--input")) {
         this.tipPercValue = parseInt(element.value) / 100;
       }
     });
   }
 
-  /*calculate tip per person amount*/
+  /**
+   * calculate tip per person amount
+   */
   calcTipAmount() {
     let totalTips = (this.billValue * this.tipPercValue) / this.peopleValue;
     if (!isNaN(totalTips)) {
@@ -117,7 +135,9 @@ class TipCalculator {
     }
   }
 
-  /*calculate total cost per person amount*/
+  /**
+   * calculate total cost per person amount
+   */
   calcTotalAmount() {
     let totalValue =
       (this.billValue * (1 + this.tipPercValue)) / this.peopleValue;
@@ -129,37 +149,57 @@ class TipCalculator {
       }
     }
   }
-
-  /*calculate everything*/
+  /**
+   * calculate everything
+   */
   calcAll() {
-    this.calcTipAmount();
-    this.calcTotalAmount();
+    document.addEventListener("click", () => {
+      this.calcTipAmount();
+      this.calcTotalAmount();
+    });
+    document.addEventListener("keyup", () => {
+      this.calcTipAmount();
+      this.calcTotalAmount();
+    });
   }
 
   /**
-   * Validate 
+   * Validate
    */
-   validate(label, input) {
+  validate(label, input) {
     document.addEventListener("keyup", (e) => {
       let element = e.target;
-      if (input.value == 0 && input.value != '') {
-          label.classList.add('-error');
-          input.classList.add('-error');
+      if (input.value == 0 && input.value != "") {
+        label.classList.add("-error");
+        input.classList.add("-error");
       } else {
-        label.classList.remove('-error');
-        input.classList.remove('-error');
+        label.classList.remove("-error");
+        input.classList.remove("-error");
       }
+    });
+  }
 
-  })
-}
+  validateAll() {
+    this.validate(this.billInputLabel, this.billInput);
+    this.validate(this.peopleInputLabel, this.peopleInput);
+  }
 
-    validateAll(){
-        this.validate(this.billInputLabel,this.billInput);
-        this.validate(this.peopleInputLabel,this.peopleInput);
-    }
+  /**
+   * toggle reset button
+   */
+  toggleResetBtn() {
+    document.addEventListener("keyup", () => {
+      if (this.billInput.value == "" && this.peopleInput.value == "") {
+        this.resetButton.classList.add("-disabled");
+      } else {
+        this.resetButton.classList.remove("-disabled");
+      }
+    });
+  }
 
-
-  /*set reset*/
+  /**
+   *set reset
+   */
   reset() {
     document.addEventListener("click", (e) => {
       let element = e.target;
@@ -175,6 +215,8 @@ class TipCalculator {
 
         this.totalPriceOutput.innerHTML = "$0.00";
         this.totalTipOutput.innerHTML = "$0.00";
+
+        this.resetButton.classList.add("-disabled");
       }
     });
   }
@@ -187,8 +229,6 @@ tipCalculator.setCustomPercTip();
 tipCalculator.setTipPerc();
 tipCalculator.setPeople();
 tipCalculator.validateAll();
+tipCalculator.toggleResetBtn();
 tipCalculator.reset();
-
-setInterval(() => {
-  tipCalculator.calcAll();
-}, 500);
+tipCalculator.calcAll();
